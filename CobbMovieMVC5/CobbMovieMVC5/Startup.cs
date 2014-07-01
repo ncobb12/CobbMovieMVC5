61d6fd61a088977d742124a3d69ba4c7ccaa6a8a
ncobb12 <ncobb@my.madonna.edu>
2014-07-01 12:51:15 -0400

    Model Changes
    Current Changes

---
+++ CobbMovieMVC5/CobbMovieMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CobbMovieMVC5.Startup))]
+namespace CobbMovieMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
